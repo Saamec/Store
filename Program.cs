5909390bb913f96275b9dfb31866f0ff0da6cd58
«Ilya <333zero777@mail.ru>
2022-10-06 16:10:29 +0300

    change class

--- Program.cs
+++ Program.cs
@@ -1,14 +1,18 @@
 ﻿
 
 StringIDProduct erger = new StringIDProduct("wefw", "wrgwg", 129, "toy");
-Warehouse.arrayStr.Add(erger);
+Warehouse<StringIDProduct>.arrayGoods.Add(erger);
+IntIDProduct tyty = new IntIDProduct(234, "wwgg", 567, "345r24");
+Warehouse<IntIDProduct>.arrayGoods.Add(tyty);
+Warehouse<Product<int>>.ADD(tyty);
 
 
-public abstract class Product<TID>
+
+public class Product<TID>
 {
-    protected TID IDProduct { get; set; }
-    protected string Name { get; set; }
-    protected int Cost { get; set; }
+    internal TID IDProduct { get; set; }
+    internal string Name { get; set; }
+    internal int Cost { get; set; }
 
     protected Product(TID idPoduct, string name, int cost)
     {
@@ -26,7 +30,7 @@ public abstract class Product<TID>
 public class StringIDProduct : Product<String>
 {
     string typeProduct { get; set; }
-    internal  StringIDProduct(string IDproduct, string Name, int Cost, string typeProduct) : base(IDproduct, Name, Cost)
+    public  StringIDProduct(string IDproduct, string Name, int Cost, string typeProduct) : base(IDproduct, Name, Cost)
     {
         this.typeProduct = typeProduct; 
     }
@@ -43,9 +47,14 @@ public class IntIDProduct : Product<int>
 
 
 
-public static class Warehouse 
+public static class Warehouse <T> where T : class
 {
-    internal static List<Product<int>> arrayInt = new List<Product<int>>();
-    internal static List<Product<string>> arrayStr = new List<Product<string>>();
+    internal static List<T> arrayGoods = new List<T>();
+
+    internal static void ADD(T value)
+    {
+        if (value is StringIDProduct) arrayGoods.Add(value);
+    }            
+
 }
 
