8a3e9f8662b5e6c1b7440bff352fde5343b63c71
«Ilya <333zero777@mail.ru>
2022-10-06 15:10:56 +0300

    progress

--- Program.cs
+++ Program.cs
@@ -1,32 +1,51 @@
 ﻿
 
+StringIDProduct erger = new StringIDProduct("wefw", "wrgwg", 129, "toy");
+Warehouse.arrayStr.Add(erger);
 
-public  class Product<TID>
+
+public abstract class Product<TID>
 {
-    public TID IDProduct { get; set; }
-    public string Name { get; set; }
-    public string Description { get; set; }
-    public int Cost { get; set; }
+    protected TID IDProduct { get; set; }
+    protected string Name { get; set; }
+    protected int Cost { get; set; }
 
-    Product(TID idPoduct, string name, string description, int cost)
+    protected Product(TID idPoduct, string name, int cost)
     {
         IDProduct = idPoduct;
         Name = name;
-        Description = description;
         Cost = cost;
+    }
+
+    protected Product()
+    {
+       
     }
 }
 
-public class Toys : Product
+public class StringIDProduct : Product<String>
 {
+    string typeProduct { get; set; }
+    internal  StringIDProduct(string IDproduct, string Name, int Cost, string typeProduct) : base(IDproduct, Name, Cost)
+    {
+        this.typeProduct = typeProduct; 
+    }
+}
 
+public class IntIDProduct : Product<int>
+{
+    string typeProduct { get; set; }
+    internal IntIDProduct(int IDproduct, string Name, int Cost, string typeProduct) : base(IDproduct, Name, Cost)
+    {
+        this.typeProduct = typeProduct;
+    }
 }
 
 
 
-
-public class Warehouse
+public static class Warehouse 
 {
-    //Product[] array = new Product[100];
-        
+    internal static List<Product<int>> arrayInt = new List<Product<int>>();
+    internal static List<Product<string>> arrayStr = new List<Product<string>>();
 }
+
