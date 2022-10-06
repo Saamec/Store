bf2827eedfa56c29012bf66440c5c1d4e3e772e5
«Ilya <333zero777@mail.ru>
2022-10-06 14:18:37 +0300

    first steps

--- Program.cs
+++ Program.cs
@@ -1,1 +1,32 @@
 ﻿
+
+
+public  class Product<TID>
+{
+    public TID IDProduct { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int Cost { get; set; }
+
+    Product(TID idPoduct, string name, string description, int cost)
+    {
+        IDProduct = idPoduct;
+        Name = name;
+        Description = description;
+        Cost = cost;
+    }
+}
+
+public class Toys : Product
+{
+
+}
+
+
+
+
+public class Warehouse
+{
+    //Product[] array = new Product[100];
+        
+}
