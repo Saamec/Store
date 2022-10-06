2d63fb1342f8ff9afa976f8ebf7a7b6981536b58
«Ilya <333zero777@mail.ru>
2022-10-06 19:25:51 +0300

    add change

--- Program.cs
+++ Program.cs
@@ -1,12 +1,28 @@
 ﻿
 
-IDProduct<string> erger = new IDProduct<string>("ПM-003", "Плюшевый мишка", 1500, "игрушка");
-IDProduct<int> erger2 = new IDProduct<int>(86, "Удивительная природа России", 2100, "книга");
+IDProduct<string> goods = new IDProduct<string>("ПM-003", "Плюшевый мишка", 1500, "игрушка");
+IDProduct<int> goods2 = new IDProduct<int>(86, "Удивительная природа России", 2100, "книга");
+IDProduct<string> goods3 = new IDProduct<string>("ПM-002", "Плюшевый чебурашка", 1100, "игрушка");
+IDProduct<string> goods4 = new IDProduct<string>("ПM-001", "Кожаный слон", 1800, "игрушка");
+IDProduct<string> goods5 = new IDProduct<string>("ПM-004", "Реплика АК-47", 2500, "игрушка");
+IDProduct<string> goods6 = new IDProduct<string>("ПM-005", "Машина Камаз", 1500, "игрушка");
 
-Warehouse<Product>.ADD(erger);
-Warehouse<Product>.ADD(erger2);
 
-Person Vasya = new Person("Вася", "Москва", "hw44tru3@mail.ru");
+Warehouse<Product>.ADD(goods);    //заполнили склад
+Warehouse<Product>.ADD(goods2);
+Warehouse<Product>.ADD(goods3);
+Warehouse<Product>.ADD(goods4);
+Warehouse<Product>.ADD(goods5);
+Warehouse<Product>.ADD(goods6);
+
+ListPersons.ADD(new Person("Василий Иванович Сургучев", "г.Москва, пр.Ленинградский, д.48, кв.17", "hw44tru3@mail.ru"));
+ListPersons.ADD(new Person("Абрамов Виталий Сергеевич", "г.Пушкин, ул. Яковлевская, д.45, кв.28", "dfhdh@mail.ru"));
+ListPersons.ADD(new Person("Камолова Антонина Петровна", "г.Санкт-Петербург, пр.Ленина, д.2, кв.354", "jljfdgc@mail.ru"));
+ListPersons.ADD(new Person("Сущев Иван Тимофеевич", "г.Донецк, ул.Спартаковская, д.93, кв.34", "4vgcdf4@mail.ru"));
+ListPersons.ADD(new Person("Задумкина Любовь Николаевна", "г.Севастополь, ул.Леваневского, д.45, кв.45", "fsdgjkifgul@mail.ru"));
+ListPersons.ADD(new Person("Никлин Валерий Сергеевич", "д. Выдрино, ул.Щеглова, д.1, кв.1", "arrrtF@mail.ru"));
+
+Person Vasya = new Person("Василий Иванович Сургучев", "г.Москва, пр.Ленинградский, д.48, кв.17", "hw44tru3@mail.ru");
 List<Product> test = Vasya.Purchase(Warehouse<Product>.arrayGoods, 2);
 Console.WriteLine(Vasya.Payment(test));
 
@@ -53,7 +69,7 @@ public class IDProduct <T>: Product
 
 
 
-public static class Warehouse <T> where T : class
+public static class Warehouse <T> where T : class            //Склад продукции
 {
     internal static List<T> arrayGoods = new List<T>();
     
@@ -63,6 +79,18 @@ public static class Warehouse <T> where T : class
         arrayGoods.Add(value);
     }            
 }
+
+public static class ListPersons                              //Список зарегистрированных заказчиков
+{
+    internal static List<Person> arrayPersons = new List<Person>();
+    
+    internal static void ADD(Person person)
+    {
+        arrayPersons.Add(person);
+    }
+
+}
+
 
 public abstract class Bayer
 {
@@ -102,11 +130,11 @@ public class Person : Bayer
     internal override string Payment(List<Product> list)
     {
         int count = 0;
-        string str = "Покупатель " + Name + "\n" +
-                     "Совершена покупка : \n";
+        string str = "|Покупатель: " + Name + "| Адрес: " + Address + "| Почта: " + Email + "\n" +
+                     "|Совершена покупка : \n|";
         foreach (Product product in list)
         {
-            str += product.Name + "\n";
+            str += product.Name + "\n|";
             count += product.Cost;
         }
         str += "На общюю сумму " + count + "\n";
@@ -114,6 +142,39 @@ public class Person : Bayer
         return str;
     }
 
-    
+    abstract class Delivery
+    {
+        public string Address;
+    }
+
+    class HomeDelivery : Delivery
+    {
+        /* ... */
+    }
+
+    class PickPointDelivery : Delivery
+    {
+        /* ... */
+    }
+
+    class ShopDelivery : Delivery
+    {
+        /* ... */
+    }
+
+    class Order<TDelivery> where TDelivery : Delivery
+    {
+        public TDelivery Delivery;
+
+        public int Number;
+
+        public Person OrderPerson(List<Person> person)
+        {
+            Random random = new Random();   
+            return person[random.Next(1, person.Count)];
+        }
+
+        
+    }
 }
 
